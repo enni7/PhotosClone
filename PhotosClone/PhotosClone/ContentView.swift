b7e8c91d83c1f45198172cb280a1be2f562b3815
enni <annaizzo7@hotmail.it>
2021-12-15 19:11:06 +0100

    added project files

---
+++ PhotosClone/PhotosClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotosClone
+//
+//  Created by Anna Izzo on 15/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotosClone/PhotosClone/PhotosCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCloneApp.swift
+//  PhotosClone
+//
+//  Created by Anna Izzo on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotosCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
